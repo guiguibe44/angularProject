c677e6a853f1a5e200b4acbad58bebc59b3cf4fa
guiguibe44 <guillaume.bex44@gmail.com>
2020-03-10 17:10:01 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { AppareilComponent } from './appareil/appareil.component';
+import { FormsModule } from '@angular/forms';
+import { AppareilService } from './services/appareil.service';
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import {RouterModule, Routes} from '@angular/router';
+
+
+const appRoutes: Routes = [
+  { path: 'appareils', component: AppareilViewComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '', component: AppareilViewComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponent,
+    AppareilComponent,
+    AuthComponent,
+    AppareilViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [AppareilService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/appareil-view/appareil-view.component.ts
+++ src/app/appareil-view/appareil-view.component.ts
@@ -1,13 +1,15 @@
-import {Component, OnInit} from '@angular/core';
-import {AppareilService} from './services/appareil.service';
+import { Component, OnInit } from '@angular/core';
+import { AppareilService } from '../services/appareil.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-appareil-view',
+  templateUrl: './appareil-view.component.html',
+  styleUrls: ['./appareil-view.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppareilViewComponent implements OnInit {
   isAuth = false;
+  appareils: any[];
+
   lastUpdate = new Promise((resolve, reject) => {
     const date = new Date();
     setTimeout(
@@ -16,21 +18,17 @@ export class AppComponent implements OnInit {
       }, 2000
     );
   });
-  appareils: any[];
 
-  constructor(private appareilService: AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-    );
-  }
+  constructor(private appareilService: AppareilService) { }
+
   ngOnInit() {
     this.appareils = this.appareilService.appareils;
   }
+
   onAllumer() {
     this.appareilService.switchOnAll();
   }
+
   onEteindre() {
     if(confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
       this.appareilService.switchOffAll();
@@ -38,6 +36,5 @@ export class AppComponent implements OnInit {
       return null;
     }
   }
-
 
 }
